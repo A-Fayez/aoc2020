b53b56c5731b9e6c98815d71bf411ef34d2615ec
a-fayez <ahmedf.higgy@gmail.com>
2021-02-03 20:25:47 +0200

    [day02] part-2 submission and small improvements

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -5,15 +5,14 @@ fn main() {
     let mut valid_passwords = (0, 0);
 
     for line in contents.lines() {
-        let t: Vec<&str> = line.split(":").collect();
-        let policy = Policy::new(t[0]);
-        let policy = policy.as_ref();
-        let password = Password(t[1].to_owned());
+        let t: Vec<&str> = line.split(": ").collect();
+        let policy = Policy::new(t[0].to_string());
+        let password = Password(t[1].to_string());
 
-        if password.is_valid_against_p1(&policy.unwrap()) {
+        if password.is_valid_against_p1(&policy) {
             valid_passwords.0 = valid_passwords.0 + 1;
         }
-        if password.is_valid_against_p2(&policy.unwrap()) {
+        if password.is_valid_against_p2(&policy) {
             valid_passwords.1 = valid_passwords.1 + 1;
         }
     }
@@ -24,28 +23,27 @@ fn main() {
 #[test]
 fn part2_test() {
     let pw = Password("pqhpppwpfmr".to_string());
-    let policy = Policy::new("2-4 p");
-    assert_eq!(pw.is_valid_against_p2(&policy.unwrap()), true);
+    let policy = Policy::new("2-4 p".to_string());
+    assert_eq!(pw.is_valid_against_p2(&policy), true);
 }
 
 struct Policy {
     boundaries: (usize, usize),
-    letter: String,
+    letter: char,
 }
 
 struct Password(String);
 
 impl Policy {
-    fn new(s: &str) -> Result<Self, &'static str> {
+    fn new(s: String) -> Self {
         let t: Vec<&str> = s.split_whitespace().collect();
-        let letter = t[1].to_owned();
-        let boundaries: Vec<&str> = t[0].split("-").collect();
-        let boundaries: Vec<usize> = boundaries
-            .iter()
+        let letter = t[1].to_string().chars().next().unwrap();
+        let boundaries: Vec<_> = t[0]
+            .split("-")
             .map(|n| n.parse::<usize>().unwrap())
             .collect();
         let boundaries: (usize, usize) = (boundaries[0], boundaries[1]);
-        Ok(Self { boundaries, letter })
+        Self { boundaries, letter }
     }
 }
 
@@ -57,12 +55,12 @@ impl fmt::Display for Password {
 
 impl Password {
     fn is_valid_against_p1(&self, policy: &Policy) -> bool {
-        let number_of_ocs = self.0.matches(&policy.letter).count();
+        let number_of_ocs = self.0.matches(policy.letter).count();
         number_of_ocs >= policy.boundaries.0 && number_of_ocs <= policy.boundaries.1
     }
     fn is_valid_against_p2(&self, policy: &Policy) -> bool {
         let pos = policy.boundaries;
-        let letter = policy.letter.chars().next().unwrap();
+        let letter = policy.letter;
 
         let a = self.0.chars().nth(pos.0 - 1) == Some(letter);
         let b = self.0.chars().nth(pos.1 - 1) == Some(letter);
