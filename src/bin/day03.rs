74ec12f64cee711fa388b8b320f3ff96ce8ecc77
a-fayez <ahmedf.higgy@gmail.com>
2021-02-07 22:08:01 +0200

    [day03] part1 submission

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,36 +1,36 @@
 const INPUT: &'static str = include_str!("inputs/day03.txt");
 fn main() {
-    let mut trees_count = 0;
-    let right = 3;
-    let down = 1;
-    let lines = INPUT.lines().collect::<Vec<&str>>();
-    let max_row = lines.len() - 1;
-    let max_col = lines[0].chars().collect::<Vec<_>>().len() - 1;
+    print!("trees: {}", count_trees(3, 1, INPUT));
+}
 
+#[test]
+fn test_count_trees() {
+    let input = include_str!("inputs/day03-test.txt");
+    let output = count_trees(3, 1, input);
+    assert_eq!(output, 7);
+}
+
+fn count_trees(right: usize, down: usize, input: &str) -> usize {
+    let mut trees_count = 0;
+    let lines = input.lines().collect::<Vec<&str>>();
+    let max_row = lines.len() - 1;
+    let max_col = lines[0].chars().collect::<Vec<_>>().len();
+    println!("max row: {}, max col: {}", max_row, max_col);
     let mut cur_col = 0;
 
     for i in 0..max_row {
         let cur_row = i + down;
-        cur_col = cur_col + right;
 
-        println!("curr col: {}", cur_col);
-        println!(
-            "line index: {}, char index: {}, char: {}",
-            cur_row,
-            cur_col,
-            lines[cur_row]
-                .chars()
-                .nth((cur_col - 1) % max_col + 1)
-                .unwrap()
-        );
-        if lines[cur_row]
-            .chars()
-            .nth((cur_col - 1) % max_col + 1)
-            .unwrap()
-            == '#'
-        {
-            trees_count = trees_count + 1;
+        cur_col = (cur_col + right - 1) % max_col + 1;
+        if cur_col % max_col == 0 {
+            if lines[cur_row].chars().nth(0).unwrap() == '#' {
+                trees_count = trees_count + 1;
+            }
+        } else {
+            if lines[cur_row].chars().nth(cur_col).unwrap() == '#' {
+                trees_count = trees_count + 1;
+            }
         }
     }
-    println!("Trees Count: {}", trees_count);
+    trees_count
 }
